2d80d496dc082c203b5ca5a9935dc7c7d56ebf19
AkianJS <akianzax@gmail.com>
2022-12-07 02:49:31 -0300

    Grid de libros en el carrito agregado

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -3,6 +3,7 @@ import Layout from "../components/layout/Layout";
 import AppContext from "../context/AppContext";
 import ProtectedRoute from "../components/ProtectedRoute";
 import CartBookCard from "../components/CartBookCard";
+import styles from "../styles/BooksCartGrid.module.css"
 
 const Cart = () => {
   const {
@@ -11,10 +12,10 @@ const Cart = () => {
   return (
     <Layout title='MercadoLibro Carrito de Compras'>
       <ProtectedRoute path='/login' myBoolean={login.success} isLoading={login.isLoading}>
-        <section className="max-w-screen-xl p-8 ml-auto mr-auto">
+        <section className={`max-w-screen-xl p-8 ml-auto mr-auto flex flex-wrap ${styles.cartGrid}`}>
           {
           login?.usuario?.carrito?.map(item => 
-          <CartBookCard key={item.isbn} cartBook={item}/>)
+          <CartBookCard key={item.libro.isbn} cartBook={item}/>)
           }
           
         </section>
