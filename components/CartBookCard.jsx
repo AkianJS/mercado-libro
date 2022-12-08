4b1a6302c86a42307dc27ec3997a807314803f83
AkianJS <akianzax@gmail.com>
2022-12-08 00:58:50 -0300

    Info nueva para el carrito

--- components/CartBookCard.jsx
+++ components/CartBookCard.jsx
@@ -1,14 +1,33 @@
 import Image from "next/image";
 import React from "react";
 import { FaMinus, FaPlus, FaTrash } from "react-icons/fa";
+import { removeOneFromCart } from "../utils/removeOneFromCart";
+import { setBookTocart } from "../utils/setBookToCart";
+import {removeAllFromCart} from "../utils/removeAllFromCart"
 
-const CartBookCard = ({ cartBook }) => {
+const CartBookCard = ({ cartBook, token, updateUserInfo }) => {
   const { libro } = cartBook;
-  console.log(cartBook);
   const author = libro.autor.map((item) => item.nombre);
   const discount = libro.descuento
     ? (libro.descuento / 100) * libro.precio
     : null;
+
+    // Funciones para añadir, restar o eliminar libros
+    const handleRemoveOne = async() => {
+      await removeOneFromCart({quantity: 1, isbn: libro.isbn, token: token })
+      updateUserInfo()
+    }
+
+    const handleAddOne = async() => {
+      await setBookTocart({quantity:1 , isbn: libro.isbn, token: token})
+      updateUserInfo()
+    }
+
+    const handleRemoveAll = async() => {
+      await removeAllFromCart({isbn: libro.isbn, token: token})
+      updateUserInfo()
+    }
+
   return (
     <div className="flex justify-center flex-wrap gap-6">
       <div className="relative w-36 h-48">
@@ -24,6 +43,7 @@ const CartBookCard = ({ cartBook }) => {
           {cartBook.libro.titulo}
         </h3>
         <p className="text-sm text-gray-600">{author}</p>
+        
         <div className="mt-4">
           {discount ? (
             <>
@@ -40,14 +60,14 @@ const CartBookCard = ({ cartBook }) => {
           )}
           <div className="mt-4 flex gap-4 justify-center items-center ">
             <p>Total: {libro.precio * cartBook.cantidad} $</p>
-            <button className="bg-black p-1">
+            <button onClick={handleRemoveOne} className="bg-black p-1">
               <FaMinus className="text-white"/>
             </button>
             <p>{cartBook.cantidad}</p>
-            <button className="bg-black p-1">
+            <button onClick={handleAddOne} className="bg-black p-1">
               <FaPlus className="text-white" />
             </button>
-            <button className="text-2xl ml-auto">
+            <button onClick={handleRemoveAll} className="text-2xl ml-auto">
               <FaTrash/>
             </button>
           </div>
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,24 +1,80 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import Layout from "../components/layout/Layout";
 import AppContext from "../context/AppContext";
 import ProtectedRoute from "../components/ProtectedRoute";
 import CartBookCard from "../components/CartBookCard";
-import styles from "../styles/BooksCartGrid.module.css"
+import styles from "../styles/BooksCartGrid.module.css";
+import { FaCaretRight } from "react-icons/fa";
 
 const Cart = () => {
   const {
     state: { login },
+    updateUserInfo,
   } = useContext(AppContext);
+
+  const cartHasSomething = useMemo(() => {
+    return login?.usuario?.carrito?.length > 0 ? true : false;
+  }, [login?.usuario?.carrito]);
+
+  const total = useMemo(() => {
+    if (login?.usuario?.carrito.length > 1) {
+      return login?.usuario?.carrito.reduce(
+        (prev, current) =>
+          prev.libro.precio * prev.cantidad +
+          current.libro.precio * current.cantidad
+      );
+    } else {
+      return (
+        login?.usuario?.carrito[0]?.libro?.precio *
+        login?.usuario?.carrito[0]?.cantidad
+      );
+    }
+  }, [login?.usuario?.carrito]);
+
   return (
-    <Layout title='MercadoLibro Carrito de Compras'>
-      <ProtectedRoute path='/login' myBoolean={login.success} isLoading={login.isLoading}>
-        <section className={`max-w-screen-xl p-8 ml-auto mr-auto flex flex-wrap ${styles.cartGrid}`}>
-          {
-          login?.usuario?.carrito?.map(item => 
-          <CartBookCard key={item.libro.isbn} cartBook={item}/>)
-          }
-          
+    <Layout title="MercadoLibro Carrito de Compras">
+      <ProtectedRoute
+        path="/login"
+        myBoolean={login.success}
+        isLoading={login.isLoading}
+      >
+        <section
+          className={`max-w-screen-xl p-6 ml-auto mr-auto flex flex-wrap ${styles.cartGrid}`}
+        >
+          {login?.usuario?.carrito?.map((item) => (
+            <CartBookCard
+              key={item.libro.isbn}
+              token={login.accessToken}
+              cartBook={item}
+              updateUserInfo={updateUserInfo}
+            />
+          ))}
+          {!cartHasSomething && (
+            <p className="w-full text-red-600 text-xl">
+              Aun no has agregado nada a tu carrito!
+            </p>
+          )}
         </section>
+        <div className="max-w-3xl ml-auto mr-auto p-6">
+          <hr className="border-t-2 mr-8 ml-8 " />
+          {cartHasSomething && (
+            <div className="w-3/4  p-4 m-auto flex flex-col justify-center items-center mt-4 border-gray-600 border-2">
+              <div className="flex justify-center items-center w-full">
+                <h4 className="text-xl font-bold">Total: </h4>
+                <p className="ml-auto font-bold">{total} $</p>
+              </div>
+              <div className="mt-4 flex flex-wrap justify-center items-center w-full bg-gray-400 p-2 pl-1 pr-1">
+                <FaCaretRight />
+                <input
+                  className="bg-gray-400 border-b-2 border-black placeholder:opacity-70 placeholder:text-black outline-none w-[30vw] max-w-sm"
+                  placeholder="Cupón de descuento"
+                  type="text"
+                />
+                <button className="bg-black text-white ml-4 p-1 pl-2 pr-2 rounded-sm">Agregar</button>
+              </div>
+            </div>
+          )}
+        </div>
       </ProtectedRoute>
     </Layout>
   );
