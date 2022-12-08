e33cbbb9f820b70fa832fecc27b621dd2d9e688d
AkianJS <akianzax@gmail.com>
2022-12-08 01:00:11 -0300

    Comienzo de la vista del perfil

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import AppContext from "../context/AppContext";
-import { useContext } from "react";
 import ProtectedRoute from "../components/ProtectedRoute";
+import styles from "../styles/Profile.module.css";
+import { useContext } from "react";
 
 const Profile = () => {
   const {
@@ -11,8 +12,21 @@ const Profile = () => {
 
   return (
     <Layout title="MercadoLibro Perfil">
-      <ProtectedRoute isLoading={login.isLoading} myBoolean={login?.success} path='/login'>
-        <div>Profile</div>
+      <ProtectedRoute
+        isLoading={login.isLoading}
+        myBoolean={login?.success}
+        path="/login"
+      >
+        <section className="p-8">
+          <div className={`relative w-full h-64 ${styles.banner}`}>
+            <h2 className="mt-4 text-4xl font-bold text-right absolute right-4 -bottom-4">
+              Mi Perfil
+            </h2>
+          </div>
+
+          <p>Nombre Completo: {login.usuario?.nombre}</p>
+          
+        </section>
       </ProtectedRoute>
     </Layout>
   );
