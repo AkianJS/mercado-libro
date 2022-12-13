e49509a82ba8fb8deaaa9222baf8d4a678adb5ce
AkianJS <akianzax@gmail.com>
2022-12-12 23:35:45 -0300

    Fix 1.3 opinar

--- components/OpinedCard.jsx
+++ components/OpinedCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const OpinedCard = ({ book }) => {
   const comentary = book.comentario?.split("\n");
   return (
-    <div className="m-auto max-w-xl flex flex-col items-center">
+    <div className="m-auto mt-4 max-w-xl flex flex-col items-center">
       <h3 className="ml-auto mr-2 text-lg text-emerald-600">{book.usuario?.nombre}</h3>
       <div className="flex flex-col items-center border-2 border-black rounded-md p-4 w-full">
         {comentary.map((item) => (
